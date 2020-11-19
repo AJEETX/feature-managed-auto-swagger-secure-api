addd978261b69b4e8ce7013dd941f6ed795d624f
ajeet <ajeetx@email.com>
2020-11-19 14:56:11 +1100

    added annotation

--- FeaturesEnabled.ClaimBased.AutoSwagger.Api/Models/LoginModel.cs
+++ FeaturesEnabled.ClaimBased.AutoSwagger.Api/Models/LoginModel.cs
@@ -1,7 +1,13 @@
-﻿namespace FeaturesEnabled.ClaimBased.AutoSwagger.Api.Core.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FeaturesEnabled.ClaimBased.AutoSwagger.Api.Core.Models
 {
     public class LoginModel
     {
+        [Required]
+        [Display(Name = "Email")]
+        [EmailAddress]
+        [RegularExpression(@"^([A-Za-z0-9][^'!&\\#*$%^?<>()+=:;`~\[\]{}|/,₹€@ ][a-zA-z0-9-._][^!&\\#*$%^?<>()+=:;`~\[\]{}|/,₹€@ ]*\@[a-zA-Z0-9][^!&@\\#*$%^?<>()+=':;~`.\[\]{}|/,₹€ ]*\.[a-zA-Z]{2,6})$", ErrorMessage = "Please enter a valid Email")]
         public string Email { get; set; }
     }
 }
