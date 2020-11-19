eb1d49ac9904f48f370da88d162dbf5fa4b4ceb2
ajeet <ajeetx@email.com>
2020-11-19 14:58:17 +1100

    removed extra email validation

--- FeaturesEnabled.ClaimBased.AutoSwagger.Api/Models/LoginModel.cs
+++ FeaturesEnabled.ClaimBased.AutoSwagger.Api/Models/LoginModel.cs
@@ -7,7 +7,6 @@ namespace FeaturesEnabled.ClaimBased.AutoSwagger.Api.Core.Models
         [Required]
         [Display(Name = "Email")]
         [EmailAddress]
-        [RegularExpression(@"^([A-Za-z0-9][^'!&\\#*$%^?<>()+=:;`~\[\]{}|/,₹€@ ][a-zA-z0-9-._][^!&\\#*$%^?<>()+=:;`~\[\]{}|/,₹€@ ]*\@[a-zA-Z0-9][^!&@\\#*$%^?<>()+=':;~`.\[\]{}|/,₹€ ]*\.[a-zA-Z]{2,6})$", ErrorMessage = "Please enter a valid Email")]
         public string Email { get; set; }
     }
 }
